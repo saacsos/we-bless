6dd9d8eee7ab1d1ffb9153494b9584d987fe910b
Saac Sornchai <sornchai.la@ku.th>
2021-09-28 00:03:56 +0700

    jwt-auth

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -7,9 +7,14 @@ use App\Http\Resources\TaskCollection;
 use App\Http\Resources\TaskResource;
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class TaskController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth:api');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -17,7 +22,8 @@ class TaskController extends Controller
      */
     public function index()
     {
-        $tasks = Task::paginate(5);
+        $user = JWTAuth::user();
+        $tasks = $user->tasks()->paginate(5);
         return TaskResource::collection($tasks);
 //        return new TaskCollection($tasks);
     }
@@ -26,11 +32,18 @@ class TaskController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return TaskResource
      */
     public function store(Request $request)
     {
-        //
+        $task = new Task();
+        $user = JWTAuth::user();
+        $task->title = $request->input('title');
+        $task->detail = $request->input('detail');
+        $task->due_date = $request->input('due_date');
+        $task->user_id = $user->id;
+        $task->save();
+        return new TaskResource($task);
     }
 
     /**
